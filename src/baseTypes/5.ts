8dc3f6f91a73c0a0d04b92649a9d782ec9491dfc
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2023-11-25 15:08:00 +0100

    ready 1-7

--- src/baseTypes/5.ts
+++ src/baseTypes/5.ts
@@ -2,6 +2,8 @@
   Як ви визначите змінну в TypeScript, яка може приймати рядок або число (union type)? 
   І так само визначте змінну, яка може приймати тільки одне з двох рядкових значень: 'enable' або 'disable' (literal type)?
 */
+type types = string | number;
+type element = "enable" | "disable";
 
-let union;
-let literal;
+let union: types;
+let literal: element;
