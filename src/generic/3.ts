e1a8073ab075fa784ff991c6633052008ce56ae3
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2023-11-26 15:33:16 +0100

    all done

--- src/generic/3.ts
+++ src/generic/3.ts
@@ -3,8 +3,27 @@
   Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
 */
 
-function merge (objA, objB) {
+/*---------------------------------------
+type Person = {
+  name: string;
+};
+
+type AdditionFields = {
+  age: number;
+};
+
+function merge<T, U>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
-export {}
+const merged = merge<Person, AdditionFields>({ name: 'Alisa' }, { age: 28 });
+
+merged.name;
+*/
+//---------------------------------------
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign({}, objA, objB);
+}
+
+export {};
