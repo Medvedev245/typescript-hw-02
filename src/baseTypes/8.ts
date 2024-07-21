c82f07a173956908806782d75f307f06961150d7
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2024-07-21 09:30:33 +0200

    add 8 task of TypeScript

--- src/baseTypes/8.ts
+++ src/baseTypes/8.ts
@@ -2,7 +2,10 @@
   Створіть тип "Gender", використовуючи union type, 
   який може містити значення "male", "female". Створіть змінну myGender цього типу.
 */
-type Gender = "male" | "female";
-const myGender: Gender;
+// type Gender = "male" | "female";
+// const myGender: Gender;
 
-export {};
+// export {};
+
+type Gender = "male" | "female";
+let myGender: Gender;
