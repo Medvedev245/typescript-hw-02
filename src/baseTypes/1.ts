7e3e789df8d992c5c7a4a32b914ca0bd5783ac64
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2024-07-25 21:08:28 +0200

    add hw 1

--- src/baseTypes/1.ts
+++ src/baseTypes/1.ts
@@ -17,7 +17,7 @@ let age: number = 50;
 let name: string = "Max";
 let toggle: boolean = true;
 let empty: null = null;
-let notInitialize: undefined;
+let notInitializ: undefined;
 let callback = (a: number): number => {
   return 100 + a;
 };
